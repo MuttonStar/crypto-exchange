2626769204f52c1d599806a3d175a00a22760deb
MuttonStar <3119270243@qq.com>
2026-06-10 21:31:46 +0800

    创建交易所api

--- main.go
+++ main.go
@@ -1,7 +1,159 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
 
-func main(){
-	fmt.Println("Working")
+	"github.com/MuttonStar/crypto-exchange2/orderbook"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	ex := NewExchange()
+
+	e.GET("/book/:market",ex.handleGetBook)
+	e.POST("/order",ex.handlePlaceOrder)
+	e.DELETE("/order/:id",ex.cancelOrder)
+
+	e.Start(":3000")
+
+}
+
+type OrderType string
+
+const (
+	MarketOrder OrderType = "MARKET"
+	LimitOrder  OrderType = "LIMIT"
+)
+
+type Market string
+
+const (
+	MarketETH Market = "ETH"
+)
+
+type Exchange struct {
+	orderbooks map[Market]*orderbook.Orderbook
+}
+
+func NewExchange() *Exchange {
+	orderbooks := make(map[Market]*orderbook.Orderbook)
+	orderbooks[MarketETH] = orderbook.NewOrderbook()
+
+	return &Exchange{
+		orderbooks: orderbooks,
+	}
+}
+
+type PlaceOrderRequest struct {
+	Type 	OrderType
+	Bid 	bool
+	Size 	float64
+	Price 	float64
+	Market	Market
+}
+
+
+type Order struct {
+	ID 			int64
+	Prcie 		float64
+	Size		float64
+	Bid 		bool
+	Timestamp 	int64
+}
+
+type OrderbookData struct {
+	TotalBidVolume 	float64
+	TotalAskVolume 	float64
+	Asks 			[]*Order
+	Bids 			[]*Order
+}
+
+func (ex *Exchange) handleGetBook(c echo.Context) error {
+	market := Market(c.Param("market"))
+	ob ,ok := ex.orderbooks[market]
+	if !ok {
+		return c.JSON(http.StatusBadRequest,map[string]any{"msg":"market nod found"})
+	}
+
+	orderbookData := OrderbookData{
+		TotalBidVolume:	ob.BidTotalVolume(),
+		TotalAskVolume: ob.AskTotalVolume(),
+		Asks: 			[]*Order{},
+		Bids: 			[]*Order{},
+	}
+
+	//遍历所有卖单
+	for _,limit := range ob.Asks() {
+		for _,order := range limit.Orders {
+			o := Order {
+				ID:				order.ID,
+				Prcie: 			limit.Price,
+				Size:			order.Size,
+				Bid:			order.Bid,
+				Timestamp: 		order.Timestamp,
+			}
+			orderbookData.Asks = append(orderbookData.Asks,&o)
+		}
+	}
+
+	//遍历所有买单
+	for _,limit := range ob.Bids() {
+		for _,order := range limit.Orders {
+			o := Order {
+				Prcie: 			limit.Price,
+				Size:			order.Size,
+				Bid:			order.Bid,
+				Timestamp: 		order.Timestamp,
+			}
+			orderbookData.Bids = append(orderbookData.Bids,&o)
+		}
+	}
+
+	return c.JSON(http.StatusOK,orderbookData)
+}
+
+// type CancelOrderRequest struct {
+// 	Bid bool
+// 	ID 	int64
+// }
+
+//取消订单
+// func (ex *Exchange) cancelOrder(c echo.Context) error {
+// 	var CancelOrderRequest CancelOrderRequest
+// 	if err := json.NewDecoder(c.Request().Body).Decode(&CancelOrderRequest); err != nil {
+// 		return err
+// 	}
+
+// 	if CancelOrderRequest.Bid {
+// 		for _,
+// 	}
+
+// 	return nil
+// }
+
+//处理下单
+func (ex *Exchange)handlePlaceOrder(c echo.Context) error {
+	var placeOrderData PlaceOrderRequest
+
+	if err := json.NewDecoder(c.Request().Body).Decode(&placeOrderData) ; err != nil {
+		return err
+	}
+
+	market := Market(placeOrderData.Market)
+	ob := ex.orderbooks[market]
+	order := orderbook.NewOrder(placeOrderData.Bid, placeOrderData.Size)
+
+	if placeOrderData.Type == LimitOrder {
+		ob.PlaceLimitOrder(placeOrderData.Price, order)
+		return c.JSON(200,map[string]any{"msg":"limit order placed"})
+	}
+
+	if placeOrderData.Type == MarketOrder {
+		matches := ob.PlaceMarketOrder(order) // 市价单无需设置价格
+		return c.JSON(200,map[string]any{"matches":len(matches)})
+	}
+
+	return nil
 }
